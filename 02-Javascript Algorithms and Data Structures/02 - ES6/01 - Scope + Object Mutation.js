e0897eab5b8762a837d34dfff97be3403694d0f3
shivkumar98 <58525809+shivkumar98@users.noreply.github.com>
2022-10-29 15:23:07 +0100

    Finished scope and object mutation of ES6 section

--- 02-Javascript Algorithms and Data Structures/02 - ES6/01 - Scope + Object Mutation.js
+++ 02-Javascript Algorithms and Data Structures/02 - ES6/01 - Scope + Object Mutation.js
@@ -42,7 +42,7 @@
           // Only change code above this line
         }
         editInPlace();
-        console.log(s)
+        // console.log(s)  // [ 2, 5, 7]
 
 // ---------------- Compare Scopes of the var and let Keywords -------------------- //
 
@@ -51,6 +51,29 @@
 
 // -------------------- Prevent Object Mutation ------------------- //
       // javascript has a method which prevents object mutation: Object.freeze
+
+      /***  Example 3:  ****/
+      /*    In this challenge you are going to use Object.freeze to prevent mathematical constants from changing. 
+            You need to freeze the MATH_CONSTANTS object so that no one is able to alter the value of PI, add, or delete properties.   */
+
+            function freezeObj() {
+              const MATH_CONSTANTS = {
+                PI: 3.14
+              };
+              // Only change code below this line
+              
+              Object.freeze(MATH_CONSTANTS)
+            
+              // Only change code above this line
+              try {
+                MATH_CONSTANTS.PI = 99;
+              } catch(ex) {
+                console.log(ex);
+              }
+              return MATH_CONSTANTS.PI;
+            }
+            const PI = freezeObj();
+            console.log(PI)
       
 // -------------------- Prevent Object Mutation ------------------- //
 
