7297c96e18f17764d19adec5c8fc5b8c416a3d5c
shivkumar98 <58525809+shivkumar98@users.noreply.github.com>
2022-10-29 16:18:58 +0100

    Corrected typo of example 1 in scope + object mutation file

--- 02 - Javascript Algorithms and Data Structures/02 - ES6/01 - Scope + Object Mutation.js
+++ 02 - Javascript Algorithms and Data Structures/02 - ES6/01 - Scope + Object Mutation.js
@@ -13,7 +13,7 @@
          function checkScope() {
             let i = 'function scope';
             if (true) {
-              let = 'block scope';
+              let i = 'block scope';
               console.log('Block scope i is: ', i);
             }
             console.log('Function scope i is: ', i);
