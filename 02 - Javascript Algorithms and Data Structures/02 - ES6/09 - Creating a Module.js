a792e420031807d69187d38e89a15637ab088e6d
shivkumar98 <58525809+shivkumar98@users.noreply.github.com>
2022-11-02 13:33:29 +0000

    Finished challenge on setting script source of a html document

---
+++ 02 - Javascript Algorithms and Data Structures/02 - ES6/09 - Creating a Module.js
@@ -0,0 +1,16 @@
+// ---------------- Creating a Module Script ---------------- \\
+
+    // ES6 introduced code sharing between JS files.
+    // This involves exporting parts of a file for use in other file
+    // E.g. <script type="module" src="filename.js"></script>
+
+    /*** Challenge: ***/
+    /* Add a script to the HTML document of type module and give it the source file of in idex.js */
+
+    <html>
+        <body>
+            <script src="index.js" type="module"></script>
+        </body>
+    </html>
+
+// ---------------- Creating a Module Script ---------------- \\
