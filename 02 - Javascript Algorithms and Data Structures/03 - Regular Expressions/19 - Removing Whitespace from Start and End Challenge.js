b00c027cc6ef34453bf0bd83ea2f69ce29a1e26b
shivkumar98 <shiv_kumar.98@outlook.com>
2022-11-04 16:22:17 +0000

    Finished Section 3 - Regular Expressions
    For JavaScript Algorithms and Data Structures

---
+++ 02 - Javascript Algorithms and Data Structures/03 - Regular Expressions/19 - Removing Whitespace from Start and End Challenge.js
@@ -0,0 +1,10 @@
+// ------------ Remove Whitespace from Start and End ------------- \\
+
+    /*** Challenge ***/
+        // write a regex to remove white space from  begging and end
+        let hello = "   Hello, World!  ";
+        let wsRegex = /^\s+|\s+$/g; // Change this line
+        let result = hello.replace(wsRegex, ""); // Change this line
+    /*** Challenge ***/
+
+// ------------ Remove Whitespace from Start and End ------------- \\
