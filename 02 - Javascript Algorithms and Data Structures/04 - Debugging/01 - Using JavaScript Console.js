b287ef19d24df4cb8555ac01494d4749b78b7364
shivkumar98 <shiv_kumar.98@outlook.com>
2022-11-04 16:45:06 +0000

    Finished Section 4 - Debugging
    For JavaScript Algorithms and Data Structures

---
+++ 02 - Javascript Algorithms and Data Structures/04 - Debugging/01 - Using JavaScript Console.js
@@ -0,0 +1,8 @@
+// ------------- Use the JavaScript Console to Check the Value of a Variable ------------- \\
+
+    // browsers allow you to debug javascript by opening the console by opening the devtools
+
+    // we can clear the messages from the console using console.clear()
+
+
+// ------------- Use the JavaScript Console to Check the Value of a Variable ------------- \\
---
+++ 02 - Javascript Algorithms and Data Structures/04 - Debugging/02 - Use typeof to Check Type of a Variable.js
@@ -0,0 +1,15 @@
+// --------------- Use typeof to Check Type of a Variable ------------- \\
+
+    // typeof lets us check the datastructure of a variable
+    // there are seven data types: Boolean, Null, Number, String, Symbol, Undefined and BigInt
+
+    /*** Example ***/
+        let seven = 7;
+        let three = "3";
+        console.log(seven + three);
+
+        console.log(typeof seven) // number
+        console.log(typeof three) // string
+    /*** Example ***/
+
+// --------------- Use typeof to Check Type of a Variable ------------- \\
