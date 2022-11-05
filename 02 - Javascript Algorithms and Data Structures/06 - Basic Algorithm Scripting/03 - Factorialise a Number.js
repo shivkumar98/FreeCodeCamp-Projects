0bb4556439d16de2539475285ec224457ab9cd0c
shivkumar98 <58525809+shivkumar98@users.noreply.github.com>
2022-11-05 12:51:45 +0000

    Rename folder for Section 6: Basic Algorithm Scripting
    For JavaScript Algorithms and Data Structures

---
+++ 02 - Javascript Algorithms and Data Structures/06 - Basic Algorithm Scripting/03 - Factorialise a Number.js
@@ -0,0 +1,12 @@
+// --------------- Factorialise a Number --------------- \
+function factorialize(num) {
+    let product = 1
+    for (let i = 0; i < num; i++){
+        product*=(i+1)
+    }
+    return product
+  }
+  
+  factorialize(5);
+    
+// --------------- Factorialise a Number --------------- \
---
+++ 02 - Javascript Algorithms and Data Structures/06 - Basic Algorithm Scripting/04 - Find the Longest Word in a String.js
@@ -0,0 +1,14 @@
+// --------------- Find the Longest Word in a String --------------- \
+function findLongestWordLength(str) {
+    let splitList = str.split(" ")
+    console.log(splitList)
+    let longest = 0
+    for (let i = 0; i < splitList.length; i ++){
+        console.log(splitList[i])
+        if splitList[i].length
+    }
+}
+  
+console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog"))
+    
+// --------------- Find the Longest Word in a String --------------- \
