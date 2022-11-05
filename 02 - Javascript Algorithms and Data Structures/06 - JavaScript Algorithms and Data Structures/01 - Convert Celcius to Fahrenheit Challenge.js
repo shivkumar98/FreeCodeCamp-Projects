70762bd0611d572a4406dd211ec6bbc076d6ab7b
shivkumar98 <58525809+shivkumar98@users.noreply.github.com>
2022-11-05 12:21:37 +0000

    Create 01 - Convert Celcius to Fahrenheit Challenge.js
    For JavaScript Algorithms and Data Structures - Section 6: JavaScript Algorithms and Data Structures

---
+++ 02 - Javascript Algorithms and Data Structures/06 - JavaScript Algorithms and Data Structures/01 - Convert Celcius to Fahrenheit Challenge.js
@@ -0,0 +1,9 @@
+// --------------- Convert Celcius to Fahrenheit Challenge --------------- \
+    // to convert celcius to farenheit: F = C * 5/9 + 32
+    function convertCtoF(celsius) {
+        let fahrenheit = celsius * (9/5) + 32;
+        return fahrenheit;
+      }
+      
+    convertCtoF(30);
+// --------------- Convert Celcius to Fahrenheit Challenge --------------- \
