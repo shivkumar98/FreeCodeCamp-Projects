13e74560ed335590d4f236b37f3a16e640eb7524
shivkumar98 <58525809+shivkumar98@users.noreply.github.com>
2022-11-05 12:14:04 +0000

    Finished Section 5 - Basic Data Structures
    For JavaScript Algorithms and Data Structures

---
+++ 02 - Javascript Algorithms and Data Structures/05 - Basic Data Structures/15 - Modify an Array Stored in an Object.js
@@ -0,0 +1,33 @@
+// --------------- Modify an Array Stored in an Object --------------- \
+
+    /*** Challenge ***/
+        // finish writing the function  so it adds the name of friend 
+        let user = {name: 'Kenneth',age: 28,
+                    data: {
+                        username: 'kennethCodesAllDay',
+                        joinDate: 'March 26, 2016',
+                        organization: 'freeCodeCamp',
+                        friends: [
+                            'Sam',
+                            'Kira',
+                            'Tomo'
+                        ],
+                        location: {
+                            city: 'San Francisco',
+                            state: 'CA',
+                            country: 'USA'
+                        }
+                    }
+                };
+        
+        function addFriend(userObj, friend) {
+            // Only change code below this line
+            userObj.data.friends.push(friend)
+            return userObj.data.friends
+            // Only change code above this line
+        }
+        
+        console.log(addFriend(user, 'Pete'));
+    /*** Challenge ***/
+    
+// --------------- Modify an Array Stored in an Object --------------- \
