c1e7e117b2e8a930a1e52b29fcc1fee05e6d7d1b
shivkumar98 <58525809+shivkumar98@users.noreply.github.com>
2022-11-05 14:41:09 +0000

    Create 08 - Truncate a String.js
    For JavaScript Algorithms and Data Structures - Section 6: Basic Algorithm Scripting

---
+++ 02 - Javascript Algorithms and Data Structures/06 - Basic Algorithm Scripting/08 - Truncate a String.js
@@ -0,0 +1,10 @@
+// --------------- Truncate a String --------------- \
+function truncateString(str, num) {
+    if (str.length > num){
+        return str.slice(0,num)+ "...";
+    } else return str
+  }
+  
+  truncateString("A-tisket a-tasket A green and yellow basket", 8);
+    
+// --------------- Truncate a String --------------- \
