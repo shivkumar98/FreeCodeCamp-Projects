a9331d6875faa5fcc97cd9bce9de689837ad5b41
shivkumar98 <58525809+shivkumar98@users.noreply.github.com>
2022-11-05 12:56:12 +0000

    Update 04 - Find the Longest Word in a String.js
    For JavaScript Algorithms and Data Structures - Section 6: Basic Algorithm Scripting

--- 02 - Javascript Algorithms and Data Structures/06 - Basic Algorithm Scripting/04 - Find the Longest Word in a String.js
+++ 02 - Javascript Algorithms and Data Structures/06 - Basic Algorithm Scripting/04 - Find the Longest Word in a String.js
@@ -1,12 +1,14 @@
 // --------------- Find the Longest Word in a String --------------- \
 function findLongestWordLength(str) {
     let splitList = str.split(" ")
-    console.log(splitList)
     let longest = 0
     for (let i = 0; i < splitList.length; i ++){
-        console.log(splitList[i])
-        if splitList[i].length
+        let stringLength = splitList[i].length
+        if (stringLength>longest){
+            longest = stringLength
+        }
     }
+    return longest
 }
   
 console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog"))
