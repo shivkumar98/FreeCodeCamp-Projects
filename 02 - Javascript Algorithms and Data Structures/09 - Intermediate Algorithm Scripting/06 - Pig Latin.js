387344e4950323a0137806c2f7cc2336231983fa
shivkumar98 <shiv_kumar.98@outlook.com>
2022-11-16 14:51:13 +0000

    Create 06 - Pig Latin.js
    For JavaScript Algorithms and Data Structures - Section 9: Intermediate Algo Scripting

---
+++ 02 - Javascript Algorithms and Data Structures/09 - Intermediate Algorithm Scripting/06 - Pig Latin.js
@@ -0,0 +1,6 @@
+// rules of pig latin:
+
+// if word begins with consonant, take the first consonant or consonant cluster and add it to the end of the word and add "ay" to it
+
+// if a word begins with a vowel, just add "way" at the end
+
