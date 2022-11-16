a801bfda90197f92be06c93737d94279053d9b4f
shivkumar98 <shiv_kumar.98@outlook.com>
2022-11-16 14:19:20 +0000

    Update 04 - Wherefore art thou.js
    For JavaScript Algorithms and Data Structures - Section 9: Intermediate Algo Scripting

--- 02 - Javascript Algorithms and Data Structures/09 - Intermediate Algorithm Scripting/04 - Wherefore art thou.js
+++ 02 - Javascript Algorithms and Data Structures/09 - Intermediate Algorithm Scripting/04 - Wherefore art thou.js
@@ -20,15 +20,22 @@ function whatIsInAName(collection, source) {
   const souceKeys = Object.keys(source);
 
   // filter the collection
-  return collection.filter(obj => {
-    for (let i = 0; i < souceKeys.length; i++) {
-      if (!obj.hasOwnProperty(souceKeys[i]) ||
-          obj[souceKeys[i]] !== source[souceKeys[i]]) {
-        return false;
+  function whatIsInAName(collection, source) {
+    // "What's in a name? that which we call a rose
+    // By any other name would smell as sweet.”
+    // -- by William Shakespeare, Romeo and Juliet
+    const souceKeys = Object.keys(source);
+  
+    // filter the collection
+    return collection.filter(obj => {
+      for (let i = 0; i < souceKeys.length; i++) {
+        if (!obj.hasOwnProperty(souceKeys[i]) ||
+            obj[souceKeys[i]] !== source[souceKeys[i]]) {
+          return false;
+        }
       }
-    }
-    return true;
-  });
+      return true;
+    });
   }
   
   whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
