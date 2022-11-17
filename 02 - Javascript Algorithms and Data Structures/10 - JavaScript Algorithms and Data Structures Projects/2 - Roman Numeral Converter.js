df8750135e15f74e688fc24f2b2e387314ffe63a
shivkumar98 <shiv_kumar.98@outlook.com>
2022-11-17 16:48:57 +0000

    Update 2 - Roman Numeral Converter.js
    For JavaScript Algorithms and Data Structures - Section 10: JavaScript Algorithms and Data Structures Projects

--- 02 - Javascript Algorithms and Data Structures/10 - JavaScript Algorithms and Data Structures Projects/2 - Roman Numeral Converter.js
+++ 02 - Javascript Algorithms and Data Structures/10 - JavaScript Algorithms and Data Structures Projects/2 - Roman Numeral Converter.js
@@ -1,19 +1,25 @@
 function convertToRoman(num) {
-    if (num<=3){
-      return "I".repeat(num)
-    } else if (num==4){
-      return "IV"
-    } else if (num>=5 && num<=8){
-      return "V"+"I".repeat(num - 5) 
-    } else if (num==9) {
-      return "IX"
-    } else if (num>=10 && num<=13){
-      return "X"+ "I".repeat(num-10)
-    } else if (num==14){
-      return "X"+convertToRoman(num-10)
-    } else if (num>=15&&num<=19){
-      
-    }
-    
-   
-   }
+  let numberOfTens = Math.floor(num/10)
+ if (num<=3){
+   return "I".repeat(num)
+ } else if (num==4){
+   return "IV"
+ } else if (num>=5 && num<=8){
+   return "V"+convertToRoman(num-5)
+ } else if (num==9) {
+   return "IX"
+ } else if (num>=10 && num<=39){
+   return "X".repeat(numberOfTens) + convertToRoman(num-10*(numberOfTens))
+ } else if (num>=40 &&num<49){
+   return "XL"+convertToRoman(num-10*(numberOfTens))
+ } else if (num>=50 && num <=59){
+   return "L"+convertToRoamn(num-10*(numberOfTens))
+ } else if (num>=60 && num<=69){
+   return "LX" + convertToRoman(num-10*(numberOfTens))
+ } else if (num>=70 && num<=79){
+   return "L" + convertToRoman(num-50)
+ } else if (num>=80 && num<=89){
+   return "L" +"X".repeat(Math.floor((num-50) / 10))+ convertToRoman(num-10*numberOfTens)
+ }
+
+}
