012532ff245eb2f398a46ae7b3571db84f603e31
shivkumar98 <shiv_kumar.98@outlook.com>
2022-11-17 10:59:48 +0000

    Update 12 - Sum All Odd Fibonacci Numbers.js
    For JavaScript Algorithms and Data Structures - Section 9: Intermediate Algo Scripting

--- 02 - Javascript Algorithms and Data Structures/09 - Intermediate Algorithm Scripting/12 - Sum All Odd Fibonacci Numbers.js
+++ 02 - Javascript Algorithms and Data Structures/09 - Intermediate Algorithm Scripting/12 - Sum All Odd Fibonacci Numbers.js
@@ -1,15 +1,24 @@
 // given a positive integer num, return the sum of all fibonacci numbers less than or equal to num
 
 function sumFibs(num) {
-    let sum = 0;
-    let fibo = [0,1]
-    let fiboLength = fibo.length
-    while (fibo[fiboLength-1]<=num){
-      fibo.push(fibo[fibo.length-2]+fibo[fibo.length-1])
+  let fibo = [0,1]
+  let fiboLength = fibo.length
+
+  while (fibo[fiboLength-1]<=num){
+    let sum = fibo[fibo.length-2]+fibo[fibo.length-1]
+    if (sum<=num){
+      fibo.push(sum)
     }
-    console.log(fibo)
-    return fibo[fibo.length-1]
   }
+  
+  console.log(fibo)
+  return fibo.filter(i=>i%2==1)
+          .reduce((sum,i)=>sum+i,0)
+          
+}
+
+
+
   
   sumFibs(4);
   
