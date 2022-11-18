af949733ba942225f4b60a26a4b84ff4f288def4
shivkumar98 <shiv_kumar.98@outlook.com>
2022-11-18 15:24:18 +0000

    Finished writing documentation+tests for Palindrome checker

---
+++ 02 - Javascript Algorithms and Data Structures/10 - JavaScript Algorithms and Data Structures Projects/1 - Palindrome Challenge/Tests.js
@@ -0,0 +1,14 @@
+console.log(palindrome("eye")) // true
+console.log(palindrome("_eye")) // true
+console.log(palindrome("race car")) // true
+console.log(palindrome("not a palindrome")) // false
+console.log(palindrome("A man, a plan, a canal. Panama")) // true 
+console.log(palindrome("never odd or even")) // true
+console.log(palindrome("nope")) // false
+console.log(palindrome("almostomla")) // false
+console.log(palindrome("My age is 0, 0 si ega ym.")) // true
+console.log(palindrome("1 eye for of 1 eye.")) // false
+console.log(palindrome("0_0 (: /-\ :) 0-0")) // true
+console.log(palindrome("five|\_/|four")) // false
+
+// ALL TESTS PASSED!
