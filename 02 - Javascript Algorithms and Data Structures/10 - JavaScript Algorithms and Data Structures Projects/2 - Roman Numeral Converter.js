798b2f754f8bbc782c2da90a4d1906d5be17e244
shivkumar98 <shiv_kumar.98@outlook.com>
2022-11-18 14:36:25 +0000

    Completed Roman Numerals Challenge
    For JavaScript Algorithms and Data Structures - Section 10: JavaScript Algorithms and Data Structures Projects

--- 02 - Javascript Algorithms and Data Structures/10 - JavaScript Algorithms and Data Structures Projects/2 - Roman Numeral Converter.js
+++ 02 - Javascript Algorithms and Data Structures/10 - JavaScript Algorithms and Data Structures Projects/2 - Roman Numeral Converter.js
@@ -3,7 +3,7 @@ function convertToRoman(num) {
   let numberOfTens = Math.floor(num/10)
   let numberOfTensPast50 = Math.floor ((num-50)/10)
   let numberOf100s = Math.floor(num/100)
-  let numberOf100sPast500 = Math.floor(num-500)
+  let numberOf1000s = Math.floor(num/1000)
  if (num<=3){
    return "I".repeat(num)
  } else if (num==4){
@@ -19,12 +19,16 @@ function convertToRoman(num) {
  } else if (num>=50 && num<=89){
    return "L" + "X".repeat(numberOfTensPast50) + convertToRoman(num-10*(numberOfTens))
  } else if (num>=90 && num<=99){ //edge case
-   return "XC" + convertToRoman(num-90)
+   return "XC" + convertToRoman(num-10*(numberOfTens))
  } else if (num>=100 && num<=399){
    return "C".repeat(numberOf100s) + convertToRoman(num-(100*numberOf100s))
  } else if (num>=400 && num<499){ // edge case
-   return "CD" + convertToRoman(num-400)
+   return "CD"  + convertToRoman(num-(100*numberOf100s))
  } else if (num>=500 && num<=899){
-   return "D" + convertToRoman(num-500)
- }
+   return "D"   + convertToRoman(num-500)
+ } else if (num>=900 && num<=999){ // edge case
+   return "CM"  + convertToRoman(num-900)
+ } else if (num>=1000 && num<=3999){
+   return "M".repeat(numberOf1000s) + convertToRoman(num-(1000*(numberOf1000s)))
+ } 
 }
