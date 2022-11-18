1f277db71bf5b8d81bb5465279afc78c57f2f331
shivkumar98 <58525809+shivkumar98@users.noreply.github.com>
2022-11-18 21:43:32 +0000

    Started Cash Register Challenge
    FCC  JS Data Strucs + Algos

---
+++ 02 - Javascript Algorithms and Data Structures/10 - JavaScript Algorithms and Data Structures Projects/5 - Cash Register/Cash Register.js
@@ -0,0 +1,36 @@
+function checkCashRegister(price, cash, cid) {
+    let cashInDraw = [...cid]
+    let keys = { "PENNY": 0,
+                 "NICKEL": 1,
+                 "DIME": 2,
+                 "QUARTER": 3,
+                 "ONE": 4,
+                 "FIVE":5,
+                 "TEN":6,
+                 "TWENTY":7,
+                 "ONE HUNDRED":8 }
+
+
+  
+    let change = cash - price;
+    console.log("change: "+change)
+    let numberOfPennies = change/0.01
+    console.log(numberOfPennies)
+    let numberOfNickles = change/0.05
+    console.log("numberOfNickles: "+ change/0.05)
+    console.log("numberOfDimes: "+ change/0.1)
+    console.log("numberOfQuarters "+change/0.25 )
+    console.log("numberOfDollars "+change/1)
+  
+    
+    
+  
+    
+    let status = "CLOSED"
+    let CIDReturn = {"status": status, change: cashInDraw};
+    console.log("status: "+ CIDReturn.status)
+    console.log("change: "+ CIDReturn.change)
+    return CIDReturn
+  }
+  
+  checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
---
+++ 02 - Javascript Algorithms and Data Structures/10 - JavaScript Algorithms and Data Structures Projects/5 - Cash Register/README.md
@@ -0,0 +1,27 @@
+# Cash Register
+
+* View my code [here](/02%20-%20Javascript%20Algorithms%20and%20Data%20Structures/10%20-%20JavaScript%20Algorithms%20and%20Data%20Structures%20Projects/5%20-%20Cash%20Register/Cash%20Register.js)
+
+* View test cases [here](/02%20-%20Javascript%20Algorithms%20and%20Data%20Structures/10%20-%20JavaScript%20Algorithms%20and%20Data%20Structures%20Projects/5%20-%20Cash%20Register/Tests.js)
+
+##  Project Details
+
+* Return true if the passed string looks like a valid US phone number.
+
+* You will be presented with a string such as 8000-692-7753 or 8oo-six427676;laskdjf. Your job is to validate or reject the US phone number. 
+
+* The area code is required. If the country code is provided, you must confirm the country code is 1
+
+* The following are valid examples:
+
+    - 555-555-5555
+
+    - (555)555-5555
+
+    - (555) 555-5555
+
+    - 555 555 5555
+
+    - 55555555555
+
+    - 1 555 555 5555
