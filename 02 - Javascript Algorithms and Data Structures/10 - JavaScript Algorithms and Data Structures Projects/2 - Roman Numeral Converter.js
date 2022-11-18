3230b0dbd204807ea88e8995e90d6425f8cb6ff5
shivkumar98 <shiv_kumar.98@outlook.com>
2022-11-18 09:46:53 +0000

    Update 2 - Roman Numeral Converter.js
    For JavaScript Algorithms and Data Structures - Section 10: JavaScript Algorithms and Data Structures Projects

--- 02 - Javascript Algorithms and Data Structures/10 - JavaScript Algorithms and Data Structures Projects/2 - Roman Numeral Converter.js
+++ 02 - Javascript Algorithms and Data Structures/10 - JavaScript Algorithms and Data Structures Projects/2 - Roman Numeral Converter.js
@@ -1,5 +1,9 @@
 function convertToRoman(num) {
+  console.log("num: "+num)
   let numberOfTens = Math.floor(num/10)
+  let numberOfTensPast50 = Math.floor ((num-50)/10)
+  let numberOf100s = Math.floor(num/100)
+  let numberOf100sPast500 = Math.floor(num-500)
  if (num<=3){
    return "I".repeat(num)
  } else if (num==4){
@@ -10,16 +14,17 @@ function convertToRoman(num) {
    return "IX"
  } else if (num>=10 && num<=39){
    return "X".repeat(numberOfTens) + convertToRoman(num-10*(numberOfTens))
- } else if (num>=40 &&num<49){
-   return "XL"+convertToRoman(num-10*(numberOfTens))
- } else if (num>=50 && num <=59){
-   return "L"+convertToRoamn(num-10*(numberOfTens))
- } else if (num>=60 && num<=69){
-   return "LX" + convertToRoman(num-10*(numberOfTens))
- } else if (num>=70 && num<=79){
-   return "L" + convertToRoman(num-50)
- } else if (num>=80 && num<=89){
-   return "L" +"X".repeat(Math.floor((num-50) / 10))+ convertToRoman(num-10*numberOfTens)
+ } else if (num>=40 &&num<=49){ //edge case
+   return "XL"+convertToRoman(num-10*(numberOfTens)) 
+ } else if (num>=50 && num<=89){
+   return "L" + "X".repeat(numberOfTensPast50) + convertToRoman(num-10*(numberOfTens))
+ } else if (num>=90 && num<=99){ //edge case
+   return "XC" + convertToRoman(num-90)
+ } else if (num>=100 && num<=399){
+   return "C".repeat(numberOf100s) + convertToRoman(num-(100*numberOf100s))
+ } else if (num>=400 && num<499){ // edge case
+   return "CD" + convertToRoman(num-400)
+ } else if (num>=500 && num<=899){
+   return "D" + convertToRoman(num-500)
  }
-
 }
