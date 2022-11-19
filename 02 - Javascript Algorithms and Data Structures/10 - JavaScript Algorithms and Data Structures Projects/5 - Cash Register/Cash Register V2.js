a663ddf840c15828b02bf5c41c0a6102d57d70f1
shivkumar98 <58525809+shivkumar98@users.noreply.github.com>
2022-11-19 16:36:40 +0000

    Update Cash Register V2.js
    Passed 4/6 tests, works correctly if there is not enough money in the drawer

--- 02 - Javascript Algorithms and Data Structures/10 - JavaScript Algorithms and Data Structures Projects/5 - Cash Register/Cash Register V2.js
+++ 02 - Javascript Algorithms and Data Structures/10 - JavaScript Algorithms and Data Structures Projects/5 - Cash Register/Cash Register V2.js
@@ -24,7 +24,7 @@ function checkCashRegister(price, cash, cid) {
   }
   console.log("total "+totalCashInDraw)
   if (totalCashInDraw<changeOfPurchase){
-    return {status:"INSUFFICIENT_FUNDS",change:cid}
+    return {status:"INSUFFICIENT_FUNDS",change:[]}
   }
 
   // assume money in draw = change due
@@ -65,6 +65,9 @@ function checkCashRegister(price, cash, cid) {
 
 
 
-console.log("return: "+JSON.stringify(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])))
+// console.log("return: "+JSON.stringify(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])))
 
 // console.log("return: "+JSON.stringify(checkCashRegister(19.5, 20, [["PENNY", 0], ["NICKEL", 0], ["DIME", 0], ["QUARTER",0], ["ONE",0], ["FIVE",0], ["TEN", 0], ["TWENTY",0], ["ONE HUNDRED",0]])))
+
+console.log(JSON.stringify(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])))
+// should return {status: "INSUFFICIENT_FUNDS", change: []}
