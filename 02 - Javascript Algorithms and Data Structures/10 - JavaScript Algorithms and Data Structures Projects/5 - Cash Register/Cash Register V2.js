16d5c4aa454133213d316ef0b888d60553c96150
shivkumar98 <58525809+shivkumar98@users.noreply.github.com>
2022-11-19 16:33:23 +0000

    Updated Cash Register to return "CLOSED" if the CID==changeDue

--- 02 - Javascript Algorithms and Data Structures/10 - JavaScript Algorithms and Data Structures Projects/5 - Cash Register/Cash Register V2.js
+++ 02 - Javascript Algorithms and Data Structures/10 - JavaScript Algorithms and Data Structures Projects/5 - Cash Register/Cash Register V2.js
@@ -1,5 +1,6 @@
 function checkCashRegister(price, cash, cid) {
 
+
   const changeOfPurchase = cash - price;
   const cashIndexes = { "PENNY": 0, "NICKEL": 1, "DIME": 2, "QUARTER": 3, "ONE": 4,"FIVE":5, "TEN":6, "TWENTY":7, "ONE HUNDRED":8 }
   const cashValues = [["PENNY", 0.01], ["NICKEL", 0.05] , ["DIME",0.1] , ["QUARTER",0.25] , ["ONE",1] , ["FIVE",5], ["TEN",10], ["TWENTY",20], ["ONE HUNDRED",100] ]
@@ -12,7 +13,25 @@ function checkCashRegister(price, cash, cid) {
     return cashValues[cashIndexes[currency]][1]
   }
 
+  
+
+  //assume there is not enough money in draw
+  let totalCashInDraw = 0
   let cashInDraw = [...cid]
+  console.log("cid: "+cashInDraw)
+  for (let i=0; i<cashInDraw.length; i++){
+    totalCashInDraw+=cashInDraw[i][1]
+  }
+  console.log("total "+totalCashInDraw)
+  if (totalCashInDraw<changeOfPurchase){
+    return {status:"INSUFFICIENT_FUNDS",change:cid}
+  }
+
+  // assume money in draw = change due
+  if (totalCashInDraw==changeOfPurchase){
+    return {status:"CLOSED", change:cid}
+  }
+  
     
   let numberOfCoinsForChange = cashValues.slice()
   // finding the amount of coins which can fit into the changeOfPurchase
@@ -21,6 +40,8 @@ function checkCashRegister(price, cash, cid) {
   }
   numberOfCoinsForChange = numberOfCoinsForChange.filter(i=>i[1]!=0).reverse()
   
+
+
   // assume change can be returned in a single denominator
   for (let i=0;i<numberOfCoinsForChange.length;i++){
     let coin = numberOfCoinsForChange[i][0]
@@ -45,3 +66,5 @@ function checkCashRegister(price, cash, cid) {
 
 
 console.log("return: "+JSON.stringify(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])))
+
+// console.log("return: "+JSON.stringify(checkCashRegister(19.5, 20, [["PENNY", 0], ["NICKEL", 0], ["DIME", 0], ["QUARTER",0], ["ONE",0], ["FIVE",0], ["TEN", 0], ["TWENTY",0], ["ONE HUNDRED",0]])))
