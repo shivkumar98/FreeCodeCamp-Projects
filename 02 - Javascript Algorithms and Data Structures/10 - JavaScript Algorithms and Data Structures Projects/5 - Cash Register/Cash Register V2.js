9f85458317821428c769c8df481af43bb9778cdc
shivkumar98 <58525809+shivkumar98@users.noreply.github.com>
2022-11-19 17:05:56 +0000

    Update Cash Register V2.js

--- 02 - Javascript Algorithms and Data Structures/10 - JavaScript Algorithms and Data Structures Projects/5 - Cash Register/Cash Register V2.js
+++ 02 - Javascript Algorithms and Data Structures/10 - JavaScript Algorithms and Data Structures Projects/5 - Cash Register/Cash Register V2.js
@@ -32,6 +32,8 @@ function checkCashRegister(price, cash, cid) {
     return {status:"CLOSED", change:cid}
   }
   
+
+
     
   let numberOfCoinsForChange = cashValues.slice()
   // finding the amount of coins which can fit into the changeOfPurchase
@@ -49,16 +51,18 @@ function checkCashRegister(price, cash, cid) {
     let valueOfCoin = getValueOfCurrency(coin)
      for (let j=1;j<=numOfCoin;j++){
       if (valueOfCoin*j == changeOfPurchase){
-        console.log("exact change found")
+        console.log("change can be returned in single denominator")
         let change = [[coin,changeOfPurchase]]
-        let ReturnedChange ={
-          status: "CLOSED",
-          change: [["QUARTER", 0.5]]
-        }
+      
         return {status: "OPEN", change: change}
       }
      }
   }
+
+    // assume change cannot be returned as exact change
+
+    // assume exact change cannot be returned
+    return {status:"INSUFFICIENT_FUNDS",change:[]}
 
 }
 
@@ -69,5 +73,12 @@ function checkCashRegister(price, cash, cid) {
 
 // console.log("return: "+JSON.stringify(checkCashRegister(19.5, 20, [["PENNY", 0], ["NICKEL", 0], ["DIME", 0], ["QUARTER",0], ["ONE",0], ["FIVE",0], ["TEN", 0], ["TWENTY",0], ["ONE HUNDRED",0]])))
 
-console.log(JSON.stringify(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])))
+// console.log(JSON.stringify(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])))
 // should return {status: "INSUFFICIENT_FUNDS", change: []}
+
+
+// console.log(JSON.stringify(checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])))
+// should return {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}.
+
+console.log(JSON.stringify(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])))
+//  should return {status: "INSUFFICIENT_FUNDS", change: []}
