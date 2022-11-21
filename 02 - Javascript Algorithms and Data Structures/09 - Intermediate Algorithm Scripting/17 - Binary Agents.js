9bd43b5169bac30609da466aa265176ff47057af
shivkumar98 <shiv_kumar.98@outlook.com>
2022-11-21 10:10:44 +0000

    Finished Binary Agents Challenge
    For JavaScript Algorithms and Data Structures - Section 10: JavaScript Algorithms and Data Structures Projects

--- 02 - Javascript Algorithms and Data Structures/09 - Intermediate Algorithm Scripting/17 - Binary Agents.js
+++ 02 - Javascript Algorithms and Data Structures/09 - Intermediate Algorithm Scripting/17 - Binary Agents.js
@@ -1,1 +1,9 @@
 // return an English translated sequence of the binary string
+
+
+function binaryAgent(str) {
+    let wordsInBinary = str.split(" ")
+    return wordsInBinary.map(word=>String.fromCharCode(parseInt(word,2))).join("")
+  }
+  
+  binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
