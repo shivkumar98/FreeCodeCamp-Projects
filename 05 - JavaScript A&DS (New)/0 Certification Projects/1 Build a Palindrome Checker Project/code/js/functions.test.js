fdb7f8e16a2f476d1daae275fd760b79cfc70288
shiv.kumar <shiv_kumar.98@outlook.com>
2024-09-25 08:22:13 +0100

    Got first test to pass

--- 05 - JavaScript A&DS (New)/0 Certification Projects/1 Build a Palindrome Checker Project/code/js/functions.test.js
+++ 05 - JavaScript A&DS (New)/0 Certification Projects/1 Build a Palindrome Checker Project/code/js/functions.test.js
@@ -1,4 +1,4 @@
-const removeWhiteSpaceAndSpecialCharacters = require('./removeWhiteSpaceAndSpecialCharacters')
+const removeWhiteSpaceAndSpecialCharacters = require('./functions');
 
 test('should return input with no changes when it contains no special characters', ()=> {
     expect(removeWhiteSpaceAndSpecialCharacters('A')).toBe('A')
