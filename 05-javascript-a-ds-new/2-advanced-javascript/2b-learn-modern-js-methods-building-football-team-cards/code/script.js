e3b12473c294a75701d3e0f34e020fe52f9fd564
shiv.kumar <shiv_kumar.98@outlook.com>
2024-10-08 08:25:40 +0100

    step 11

--- 05-javascript-a-ds-new/2-advanced-javascript/2b-learn-modern-js-methods-building-football-team-cards/code/script.js
+++ 05-javascript-a-ds-new/2-advanced-javascript/2b-learn-modern-js-methods-building-football-team-cards/code/script.js
@@ -4,3 +4,5 @@ const worldCupYear = document.getElementById("year");
 const headCoach = document.getElementById("head-coach")
 
 const playerCards = document.getElementById("player-cards")
+
+const playersDropdownList = document.getElementById("players")
