b7f4222fa71e1ab9ff669a4430032cbd8d491c37
shiv.kumar <shiv_kumar.98@outlook.com>
2024-10-09 16:22:09 +0100

    20

--- 05-javascript-a-ds-new/2-advanced-javascript/2b-learn-modern-js-methods-building-football-team-cards/code/script.js
+++ 05-javascript-a-ds-new/2-advanced-javascript/2b-learn-modern-js-methods-building-football-team-cards/code/script.js
@@ -178,3 +178,8 @@ players: [
 Object.freeze(myFavoriteFootballTeam)
 const { sport, team, year, players } = myFavoriteFootballTeam;
 const { coachName } =  myFavoriteFootballTeam.headCoach 
+
+typeOfSport.textContent = sport
+teamName.textContent = team
+worldCupYear.textContent = year
+headCoach.textContent = coachName
